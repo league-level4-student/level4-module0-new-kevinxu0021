10e9c5c63ae8fd671621946969c355b0ea3836fe
kevinxu0021 <32040253+kevinxu0021@users.noreply.github.com>
2021-10-25 16:57:08 -0700

    Data Type Option Pane

--- src/_04_Data_Type_Option_Pane/DataTypeOptionPane.java
+++ src/_04_Data_Type_Option_Pane/DataTypeOptionPane.java
@@ -2,6 +2,11 @@ package _04_Data_Type_Option_Pane;
 
 import javax.swing.JOptionPane;
 
+enum DataType {
+	BYTE, SHORT, INT, LONG, FLOAT, DOUBLE, BOOLEAN, CHAR
+	
+}
+
 public class DataTypeOptionPane {
     public static void main(String[] args) {
 
