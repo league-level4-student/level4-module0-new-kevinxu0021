ef207625a96b8e3d227cf9769a34227cb19a9ea2
kevinxu0021 <32040253+kevinxu0021@users.noreply.github.com>
2021-11-08 16:58:12 -0800

    Update DataTypeOptionPane.java

--- src/_04_Data_Type_Option_Pane/DataTypeOptionPane.java
+++ src/_04_Data_Type_Option_Pane/DataTypeOptionPane.java
@@ -3,13 +3,23 @@ package _04_Data_Type_Option_Pane;
 import javax.swing.JOptionPane;
 
 enum DataType {
-	BYTE, SHORT, INT, LONG, FLOAT, DOUBLE, BOOLEAN, CHAR
-	
+	Byte(8), Short(16), Int(32), Long(64), Float(32), Double(64), Boolean(1), Char(16);
+
+	private final int bits;
+
+	private DataType(int bits) {
+		this.bits = bits;
+	}
+
+	public int get() {
+		return this.bits;
+	}
 }
 
 public class DataTypeOptionPane {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        DataType[] dataTypes;
-    }
+		DataType[] dataTypes = DataType.values();;
+		JOptionPane.showOptionDialog(null, "Pick a data type to learn about!", "Data Types", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, dataTypes, null);
+	}
 }
